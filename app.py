2550275fc5e8d45960603cd9901266f94ab17bf3
przemyslawskok <przemyslaw.jump@gmail.com>
2022-07-15 17:38:24 +0200

    :)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,session
 from flask_recaptcha import ReCaptcha
 import sqlite3
 import functions as f
@@ -27,7 +27,7 @@ f.create_db()
 
 @app.route('/')
 def glowna():
-  
+    f.add_page_entry()
     return render_template("/glowna.html")
 
 
@@ -41,16 +41,45 @@ def contact_post():
             f.add_message(request)
             return redirect ("/")
         else:
+            
             print("Captcha error")
 
             return redirect ("/")
     return redirect ("/")
     
+@app.route('/login')
+def login():
+    return render_template("/admin_login.html")
 
+@app.route('/login',methods=["POST"])
+def handle_login_post():
+    if f.check_login(request):
+        session['kajhsd789asdSD&*(AS09hj']="x25s3a23153as"
+        return redirect("/admin")
+    else:
+        return render_template("/admin_login_fail.html")
+@app.route('/admin')
+def admin():
+    if f.check_if_admin_logged_in(session):
+        number_of_entries=f.select_number_of_entries()
+        return render_template("/admin_panel.html",
+        number_of_entries=number_of_entries,
+        messages=f.select_messages())
+    else:
+        return redirect("/login")
 
+@app.route('/dezaktywuj_wiadomosc_<id>')
+def dezaktywuj_wiadomosc(id):
+    if f.check_if_admin_logged_in(session):
+        f.deactivate_message(id)
+        return redirect("/admin")
+    else:
+        return redirect("/login")
+
+  
 @app.route('/blog')
 def blog():
     return render_template("/blog.html")
 
 
-app.run('0.0.0.0',port=230,debug=True)
+app.run('0.0.0.0',port=240,debug=True)
--- functions.py
+++ functions.py
@@ -1,7 +1,47 @@
 import sqlite3
-
+import datetime
+import dane
 #database functions
-
+def check_if_admin_logged_in(session):
+    if 'kajhsd789asdSD&*(AS09hj' in session:
+        return True
+    else:
+        return False
+def select_messages():
+    db=sqlite3.connect('database.db')
+    cursor=db.cursor()
+    cursor.execute("""SELECT * FROM messages ORDER BY id DESC""")
+    messages=cursor.fetchall()
+    db.close()
+    return messages
+def deactivate_message(id):
+    db=sqlite3.connect('database.db')
+    cursor=db.cursor()
+    cursor.execute("""UPDATE messages SET deleted='1' WHERE id=?""",(id,))
+    db.commit()
+    db.close()
+    return True
+def select_number_of_entries():
+    db=sqlite3.connect('database.db')
+    cursor=db.cursor()
+    cursor.execute("""SELECT number FROM entry_counter""")
+    number=cursor.fetchall()
+    db.close()
+    return number[0][0]
+def add_page_entry():
+    db=sqlite3.connect('database.db')
+    cursor=db.cursor()
+    try:
+        cursor.execute("""SELECT number FROM entry_counter""")
+        number=cursor.fetchall()
+        number=int(number[0][0])
+        number+=1
+        cursor.execute("""UPDATE entry_counter SET number=? WHERE id=1""",(number,))
+    except:
+        cursor.execute("""INSERT INTO entry_counter(number) VALUES(?)""",(1,))
+    db.commit()
+    db.close()
+    return number
 def create_db():
     db=sqlite3.connect('database.db')
     cursor=db.cursor()
@@ -11,14 +51,39 @@ def create_db():
         email_contact TEXT,
         phone_contact TEXT,
         date TEXT,
+        hour TEXT,
         deleted TEXT
         )""")
     db.commit()
     db.close()
-    return
 
+    db=sqlite3.connect('database.db')
+    cursor=db.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS entry_counter(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        number TEXT)""")
+    db.commit()
+    db.close()
+    return
+def check_login(request):
+    login=request.form['login']
+    password=request.form['password']
+    if str(login)==str(dane.ADMIN_LOGIN) and str(password)==str(dane.ADMIN_PASSWORD):
+        return True
+    else:
+        return False
 def add_message(request):
+
+
+    
     email=request.form.get('email')
     phone=request.form.get('phone')
     message=request.form.get('message')
     print(email,phone,message)
+    #add to database
+    db=sqlite3.connect('database.db')
+    cursor=db.cursor()
+    cursor.execute("""INSERT INTO messages(content,email_contact,phone_contact,date,hour,deleted) VALUES(?,?,?,?,?,?)""",(message,email,phone,datetime.datetime.now().strftime("%Y-%m-%d"),datetime.datetime.now().strftime("%H:%M:%S"),'0'))
+    db.commit()
+    db.close()
+    return True
